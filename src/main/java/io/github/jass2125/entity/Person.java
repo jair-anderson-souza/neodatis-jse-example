8176bea839e06e1726dc215fb8515e304b514ff9
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-21 14:48:21 -0300

    moving entities to specified packages

--- src/main/java/io/github/jass2125/entity/Person.java
+++ src/main/java/io/github/jass2125/entity/Person.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.github.jass2125;
+package io.github.jass2125.entity;
 
 import java.io.Serializable;
 import java.util.Objects;
--- src/main/java/io/github/jass2125/entity/Student.java
+++ src/main/java/io/github/jass2125/entity/Student.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.github.jass2125;
+package io.github.jass2125.entity;
 
 /**
  * @author Anderson Souza <jair_anderson_bs@hotmail.com>
